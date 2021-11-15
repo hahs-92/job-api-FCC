e8a5740e158038ea855dcaa3adc55fc1e4855362
Hugo Alexander Hernandez Serrano <alexandre_2420@outlook.com>
2021-11-15 10:44:35 -0500

    register user - mongoose middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,14 @@
+const { StatusCodes } = require('http-status-codes')
+//modesl
+const User = require('../models/User')
+
+
+
 const register = async (req, res) => {
-    res.send('register user')
+
+    const user = await User.create({...req.body})
+
+    res.status(StatusCodes.CREATED).json({user})
 }
 
 
