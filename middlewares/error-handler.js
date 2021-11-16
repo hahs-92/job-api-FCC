853a39d6858ec5de643c84f10c4d1c968fb9c06a
Hugo Alexander Hernandez Serrano <alexandre_2420@outlook.com>
2021-11-16 14:55:47 -0500

    fixed errorhandler

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,12 +1,39 @@
 const { StatusCodes } = require('http-status-codes')
 //errors
-const { CustomAPIError } = require('../errors')
+// const { CustomAPIError } = require('../errors')
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-    if (err instanceof CustomAPIError) {
-        return res.status(err.statusCode).json({ msg: err.message })
+
+    let customError = {
+        statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+        msg: err.message || 'Something went wrong, try again later'
     }
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+
+    // if (err instanceof CustomAPIError) {
+    //     return res.status(err.statusCode).json({ msg: err.message })
+    // }
+
+    //si no se sumistran los valores en register
+    if(err.name === 'ValidationError') {
+        customError.msg = Object.values(err.errors)
+            .map((item) => item.message)
+            .join(',')
+    }
+
+    //error arojado por mongoose
+    //si ya existe el email
+    if(err.code && err.code === 11000) {
+        customError.statusCode = 400
+        customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} field, please choose another value`
+    }
+
+    //si el id no es de tipo ID
+    if(err.name === 'CastError') {
+        customError.statusCode = 404
+        customError.msg= `No item found with id: ${ err.value}`
+    }
+
+    return res.status(customError.statusCode).json({ msg: customError.msg })
 }
 
 module.exports = errorHandlerMiddleware
