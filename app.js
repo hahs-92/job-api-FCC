f0e1b15aea358275b4d184723347e609024b5e14
Hugo Alexander Hernandez Serrano <alexandre_2420@outlook.com>
2021-11-16 17:00:05 -0500

    add security

--- app.js
+++ app.js
@@ -12,12 +12,25 @@ const routeJob = require('./routes/jobs')
 // error handler
 const notFoundMiddleware = require('./middlewares/not-found')
 const errorHandlerMiddleware = require('./middlewares/error-handler')
-
+//extra security
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
 
 const app = express()
 
-
+//middlewares
+app.set('trust proxy', 1)
+app.use(rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100 // limit each IP to 100 requests per windowMs
+}))
 app.use(express.json())
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+
 
 // routes
 app.get('/', (req, res) => {
